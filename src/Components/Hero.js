de084d4c8c33d5062b9e1ca5f2ec961ed62b153f
Poojan <poojangoyani@gmail.com>
2023-05-28 03:07:26 +0530

    Navbar-added

---
+++ src/Components/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Hero.css';
+const Hero = () => {
+  return (
+    <div id='main-hero'>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rhoncus augue vitae fringilla feugiat. Nullam nec porta nulla, id dictum ligula. Aenean nec feugiat turpis. Maecenas mauris nulla, viverra vel vulputate eget, venenatis ac sem. Ut scelerisque, diam a mattis scelerisque, felis enim ullamcorper metus, eu sagittis ligula odio nec dui. Cras interdum enim vel suscipit venenatis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam ultricies justo quam, id dignissim tellus iaculis vel. Praesent mollis sapien at nibh dapibus, at scelerisque est convallis. Fusce non fermentum nibh. Poojan
+
+
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/Components/Navbar.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import './NavbarStyles.css';
+import Logo from "../Assets/text-logo.png";
+import OverlayLogo from "../Assets/og-logo.png";
+
+const Navbar = () => {
+    const [navbarOpen, setNavbarOpen] = useState(false);
+
+    return (
+
+        <div id='nav-main-container'>
+            <div id='line'></div>
+            <nav id='navbar-tag'>
+                <a href='#' className='grid-item'>
+                    <img id='nav-logo' src={Logo} alt="" />
+                </a>
+
+                <div id='nav-links-div' className='grid-item'>
+                    <ul id='link-list' className={navbarOpen ? "#link-list active" : "#link-list"}>
+                        <li>
+                            <img id='overlay-logo' className={navbarOpen ? "#overlay-logo active" : "#overlay-logo"} src={OverlayLogo} alt="" />
+                        </li>
+                        <li>
+                            <a href='#' className='active'>Home</a>
+                        </li>
+                        <li>
+                            <a href='#'>Events</a>
+                        </li>
+                        <li>
+                            <a href='#'>Team</a>
+                        </li>
+                        <li>
+                            <a href='#'>Gallery</a>
+                        </li>
+                        <li>
+                            <a href='#'>Contact</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div id='social-div' >
+                    <ul id='social-list' className={navbarOpen ? "#social-list active" : "#social-list"}>
+                        <li>
+                            <a href='#'>
+                                <i class="fa-brands fa-instagram"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href='#'>
+                                <i class="fa-brands fa-youtube"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href='#'>
+                                <i class="fa-brands fa-facebook-f"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href='#'>
+                                <i class="fa-solid fa-envelope"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+
+
+            </nav >
+            <div id='mobile-hamburger' className={navbarOpen ? "#mobile-hamburger active" : "#mobile-hamburger"} onClick={() => setNavbarOpen((prev) => !prev)} >
+
+                {navbarOpen ? <i id="bar" className='fas fa-times'></i> : <i id="bar" className='fas fa-bars'></i>}
+
+            </div>
+        </div >
+    )
+}
+
+
+export default Navbar
