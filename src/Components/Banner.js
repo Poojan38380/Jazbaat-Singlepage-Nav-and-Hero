f783a30f397684f6836e011c5a2d73a3e7daf2ff
Poojan <poojangoyani@gmail.com>
2023-05-30 17:07:00 +0530

    About section refined but not complete

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -4,7 +4,25 @@ import bannerimg from "../Assets/banner-img.jpeg";
 const Banner = () => {
     return (
         <div id='main-banner'>
-            <img id='banner-bg' className='banner-bg' src={bannerimg} alt="" />
+            {/* <img id='banner-bg' className='banner-bg' src={bannerimg} alt="" /> */}
+            <div id='banner-text'> 
+            <div id='about'>
+            About <br></br>
+            Us
+            </div>
+            <div id='about-desc'>
+                
+            <p>The biggest cultural club of the institute which provides a platform to the students to explore all the areas of dramatics like stage plays, street plays, mono-acts and mimes.
+            </p>
+            <p>
+                Apart from acting we also work on areas like scripting, music, instruments etc. which is an integral part of drama.
+            </p> 
+            <p>
+            We have something to offer to everybody and this is the reason our family keeps growing every year and we attain new heights.
+            </p> 
+            
+            </div>
+            </div>
         </div>
     )
 }
